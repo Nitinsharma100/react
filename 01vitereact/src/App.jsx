1366d18805f5667661f71c682a9df70012597374
unknown <ns651143@gmail.com>
2024-10-08 08:52:34 +0530

    Added untracked files

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+
+    <h1>Hello react | i am learn today is the product</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit excepturi illum incidunt est aperiam exercitationem autem, commodi velit veritatis at, explicabo alias! Obcaecati placeat, quis voluptatum iure consequatur officiis quisquam!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, placeat.</p>
+    <h1>hello</h1>
+    </>
+  );
+}
+
+export default App
